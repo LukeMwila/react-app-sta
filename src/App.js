ebbbec3a0172076200a3e1d377ac5615bea97b12
Lukonde Mwila <outlier.developer@gmail.com>
2022-06-03 15:52:16 +0200

    feature: new application release v0.1.3

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a basic application for SITA.
+          This is a basic application for SITA in a container.
         </p>
-        <p>v0.1.2</p>
+        <p>v0.1.3</p>
       </header>
     </div>
   );
